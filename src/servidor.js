5042ebd9394219b3d6d93258f362b1ee77ac815e
bragaus <mannoplay@gmail.com>
2020-09-30 19:56:36 -0300

    retirei uma função que redireciona para https (não é necesária para  este projeto)

--- src/servidor.js
+++ src/servidor.js
@@ -8,14 +8,6 @@ const configs = {
     port: process.env.PORT || 3000
 }
 
-if (configs.forcarHTTPS) //Se o redirecionamento HTTP estiver habilitado, registra o middleware abaixo
-    app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
-        // if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP
-        //     res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS
-        // else //Se a requisição já é HTTPS
-            next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
-    });
-
 app.use(express.static(configs.caminho)); //Serve os outros arquivos, como CSSs, Javascripts, Imagens etc.
 
 app.get("*", (req, res) => {// O wildcard '*' serve para servir o mesmo index.html independente do caminho especificado pelo navegador.
